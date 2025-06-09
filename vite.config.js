d521e1cf4926187e44a7ef4f5cb1a81681501b29
Kezia West <40695445+KGwest@users.noreply.github.com>
2025-06-09 00:17:36 -0500

    Update vite.config.js
    Game file did not load on simplified portfolio site. Added 'base: '/g-u-e-s-s/' '

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/g-u-e-s-s/', // <--- This line is essential for GitHub Pages!
 })
